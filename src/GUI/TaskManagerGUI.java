1614f8f9ed8418de9dcff9e864f139451aa28c70
Vodenkyss <martinvodenka523@gmail.com>
2025-04-13 16:08:38 +0200

    pridani remove tlacitka, pridani FlatLaf library pro hezci vzhled

--- src/GUI/TaskManagerGUI.java
+++ src/GUI/TaskManagerGUI.java
@@ -6,6 +6,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class TaskManagerGUI {
 
@@ -17,14 +19,26 @@ public class TaskManagerGUI {
     private DefaultListModel<Task> taskDefaultListModel= new DefaultListModel<>();
 
     public void initialize(){
+
+        try {
+            UIManager.setLookAndFeel(new com.formdev.flatlaf.FlatLightLaf());
+            windowColors();
+
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
         frame = new JFrame("Finito!");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 500);
+
         frame.setLayout(new BorderLayout());
 
         JPanel bottomPanel= new JPanel(new BorderLayout());
+
         textfield = new JTextField();
         JButton addButton = new JButton("Add task");
+        JButton removeButton = new JButton("Remove");
         addButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -32,13 +46,30 @@ public class TaskManagerGUI {
             }
         });
 
+        removeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                removeTask();
+            }
+        });
+
         taskDefaultListModel = new DefaultListModel<>();
         taskJList = new JList<>(taskDefaultListModel);
         taskJList.setCellRenderer(new TaskRender());
+        taskJList.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int index = taskJList.locationToIndex(e.getPoint());
+                Task task = taskDefaultListModel.get(index);
+                task.toggleDone();
+                taskJList.repaint();
+            }
+        });
 
 
         bottomPanel.add(textfield, BorderLayout.CENTER);
         bottomPanel.add(addButton, BorderLayout.EAST);
+        bottomPanel.add(removeButton,BorderLayout.SOUTH);
 
         frame.add(bottomPanel, BorderLayout.SOUTH);
         frame.add(new JScrollPane(taskJList),BorderLayout.CENTER);
@@ -55,6 +86,32 @@ public class TaskManagerGUI {
             taskDefaultListModel.addElement(task);
             textfield.setText("");
         }
+    }
+
+    private void removeTask(){
+        Task selectedTask = taskJList.getSelectedValue();
+        if (selectedTask!=null){
+            taskDefaultListModel.removeElement(selectedTask);
+        }
+    }
+
+    private void windowColors(){
+        UIManager.put("Component.arc", 10);
+        UIManager.put("Button.arc", 15);
+        UIManager.put("TextComponent.arc", 10);
+
+        UIManager.put("Button.background", new Color(139, 217, 142));
+        UIManager.put("Button.foreground", new Color(0, 0, 0));
+
+        UIManager.put("Panel.background", new Color(81, 169, 126));
+        UIManager.put("List.background", new Color(81, 169, 126));
+        UIManager.put("List.selectionBackground", new Color(81, 169, 126));
+        UIManager.put("List.selectionForeground", Color.BLACK);
+
+        UIManager.put("TextField.background", new Color(255, 255, 255));
+        UIManager.put("TextField.foreground", Color.BLACK);
+        UIManager.put("TextField.caretForeground", new Color(33, 66, 44));
+
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import GUI.TaskManagerGUI;
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        TaskManagerGUI tmg = new TaskManagerGUI();
-        tmg.initialize();
+        TaskManagerGUI taskManagerGUI = new TaskManagerGUI();
+        taskManagerGUI.initialize();
     }
 }
