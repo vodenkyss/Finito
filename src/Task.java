745d7c4f09febac6098434e4d70381bc31437451
Vodenkyss <martinvodenka523@gmail.com>
2025-04-13 13:23:21 +0200

    Trida Task predstavujici ukol

---
+++ src/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void toggleDone() {
+        this.isDone = !this.isDone;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "description='" + description + '\'' +
+                ", isDone=" + isDone +
+                '}';
+    }
+}
